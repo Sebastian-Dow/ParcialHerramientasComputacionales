9db264abbc0846e3ceabd15521946205735a1ac8
Sebastian-Dow <94850342+Sebastian-Dow@users.noreply.github.com>
2021-11-28 23:03:06 -0500

    Update parcialfinal.py

--- parcialfinal.py
+++ parcialfinal.py
@@ -3,12 +3,21 @@
 ###Sebastin Dow Valenzuela
 ###Código: 8950987
 
+""" La siguiente función se utiliza para acotar las entradas y prevenir
+los distintoserrores que puedan surgir en la ejecución del programa. 
+Esta función recibe como argumentos cadenas que corresponden a la entrada 
+a ser verificada y a una clave que definirá de qué tipo sebe ser esa 
+clave y retornará un único argumento correspondiente a una cadena con 
+la entrada más apropiada. """
+
 def validar(cad, clas):
-    profesor = ('profesor', 'Profesor', 'PROFESOR')
+    profesor = ('profesor', 'Profesor', 'PROFESOR')     # Variantes de los roles que se admiten
     estudiante = ('estudiante', 'Estudiante', 'ESTUDIANTE')
     ans = ''
     end = False
-
+    
+    ### Prueba de verificación para entradas que deberían ser números enteros
+    
     if clas == 'num':
         while end == False:
             try: 
@@ -17,7 +26,9 @@ def validar(cad, clas):
                 end = True
             except ValueError:
                 cad = input("Digite un valor válido (número entero sin espacios o símbolos): ")
-
+    
+    ### Prueba de verificación para entradas que deberían ser roles admitidos
+    
     if clas == 'rol':
         while end == False:
             if cad in profesor:
@@ -30,6 +41,10 @@ def validar(cad, clas):
                 cad = str(input("Digite un rol válido (profesor o estudiante): "))
 
     return ans
+    
+""" La siguiente función desarrolla el proceso de solicitar y almacenar
+los datos, realizar operaciones con ellos y finalmente retornar el 
+mensaje esperado. """
 
 def compra():
     ans = ''
@@ -38,10 +53,15 @@ def compra():
     cant = 0
     valor = 0
 
+    ### Los siguiente inputs corresponden a los datos que inicialmente se solicitan:
+
     print("Por favor, ingrese los siguientes datos:")
     cedula = validar(input("Cédula: "), 'num')
     rol = validar(input("rol (Profesor o estudiante): "), 'rol')
     ans += 'El ' + str(rol) + ' con cedula ' + str(cedula)
+    
+    ### Ciclo que permite almacenar en diccionarios, múltiples productos con sus
+    ### respectivos precios y cantidades:
 
     while registrando == True:
         codigo = input("Código del producto: ")
@@ -53,6 +73,8 @@ def compra():
 
         if end == 'e':
             registrando = False
+            
+    ### Operaciones que definen el valor a pagar aplicando el descuento:
 
     for e in productos:
         if rol == 'profesor':
@@ -61,6 +83,8 @@ def compra():
             valor += int(productos[e][0])*int(productos[e][1])*1/2
 
     ans += ' debe pagar ' + str(valor) + ' por ' 
+    
+    ### Finalmente, se determina un mensaje para un único o varios productos:
 
     for i in range(len(productos)):
         cant += 1
